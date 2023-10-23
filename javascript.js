67b2e05eade2dd7c00279eba1c52bc12d8f8dbb9
SergioGaitan27 <sergiogaitan203@gmail.com>
2023-10-23 17:38:58 -0600

    Agregue los articulos en el index y se crean solos desde el js

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const menuMobile = document.querySelector('.mobile-menu');
 const menuHamMobile = document.querySelector('.menu');
 const menuCarrito = document.querySelector('.product-detail');
 const iconoCarrito = document.querySelector('.navbar-shopping-cart');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 navEmail.addEventListener('click', toggleMenuDesktop);
@@ -27,4 +28,61 @@ function toggleMenuCarrito(){
         menuMobile.classList.toggle('inactive');
     }
     menuCarrito.classList.toggle('inactive');
+}
+
+let productList = [];
+
+productList.push({
+    name: 'Bici',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Computadora',
+    price: 5000,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+productList.push({
+    name: 'Televisión',
+    price: 200,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+tarjetasProductos(productList);
+
+function tarjetasProductos(productos){
+    for(producto of productos){
+
+        let producCard = document.createElement('div');
+        producCard.classList.add('product-card');
+
+        let imgProducto = document.createElement('img');
+        imgProducto.setAttribute('src', producto.image);  
+
+        let productCardInfo = document.createElement('div');
+        productCardInfo.classList.add('product-info');
+
+        let descriptionCard = document.createElement('div');
+        let priceCard = document.createElement('p');
+        priceCard.innerText = '$' + producto.price;
+        let nameCard = document.createElement('p');
+        nameCard.innerText = producto.name;
+        descriptionCard.appendChild(priceCard);
+        descriptionCard.appendChild(nameCard);
+
+        let imgCard = document.createElement('figure');
+        let imgCarrito = document.createElement('img');
+        imgCarrito.setAttribute('src', './icons/bt_add_to_cart.svg');
+        imgCard.appendChild(imgCarrito);
+
+        productCardInfo.appendChild(descriptionCard);
+        productCardInfo.appendChild(imgCard);
+
+        producCard.appendChild(imgProducto);
+        producCard.appendChild(productCardInfo);
+
+        cardsContainer.appendChild(producCard);
+    }
 }
