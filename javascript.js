32fd1978985bcf6e17f53f5d769c9486393c859c
SergioGaitan27 <sergiogaitan203@gmail.com>
2023-10-22 23:42:58 -0600

    Agregue menu mobile y media para ocultarse responsivamente ambos menus

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const menuHamMobile = document.querySelector('.menu');
+
 
 navEmail.addEventListener('click', toggleMenuDesktop);
+menuHamMobile.addEventListener('click', toggleMobileDesktop);
 
 function toggleMenuDesktop(){
-    console.log('Click de la función');
     menuDesktop.classList.toggle('inactive');
+}
+
+function toggleMobileDesktop(){
+    menuMobile.classList.toggle('inactive');
 }
